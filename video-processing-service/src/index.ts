68337d151f54baed66cac2a2156fafd2a82efe6f
RainyLeonhardt <arthur3048@gmail.com>
2023-09-13 09:45:38 -0700

    Commit message describing the changes

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,11 +1,31 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
+app.get("/process-video", (req, res) => {
+    // Get path of the input video file from the request body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request: Missing File Path.");
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360") // 360p
+        .on("end", () =>{
+            res.status(200).send("Processing finished successfully.")
+        })
+        .on("error", (err) => {
+            console.log(`An error occured: ${err.message}`);
+            res.status(500).send(`Internal Server Error. ${err.message}`);
+        })
+        .save(outputFilePath);
 });
+
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Video processing service listening at http://localhost:${port}`);
