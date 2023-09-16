156b638d617b1fa3da0546e721e2bd0e6100617d
RainyLeonhardt <arthur3048@gmail.com>
2023-09-16 13:08:24 -0700

    Commit message describing the changes

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,32 +1,35 @@
-import express from "express";
-import ffmpeg from "fluent-ffmpeg";
+import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
+app.use(express.json());
 
-app.get("/process-video", (req, res) => {
-    // Get path of the input video file from the request body
-    const inputFilePath = req.body.inputFilePath;
-    const outputFilePath = req.body.outputFilePath;
+app.post('/process-video', (req, res) => {
 
-    if (!inputFilePath || !outputFilePath) {
-        res.status(400).send("Bad Request: Missing File Path.");
-    }
+  // Get the path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
 
-    ffmpeg(inputFilePath)
-        .outputOptions("-vf", "scale=-1:360") // 360p
-        .on("end", () =>{
-            res.status(200).send("Processing finished successfully.")
-        })
-        .on("error", (err) => {
-            console.log(`An error occured: ${err.message}`);
-            res.status(500).send(`Internal Server Error. ${err.message}`);
-        })
-        .save(outputFilePath);
+  // Check if the input file path is defined
+  if (!inputFilePath || !outputFilePath) {
+    return res.status(400).send('Bad Request: Missing file path');
+  }
+
+  // Create the ffmpeg command
+  ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360') // 360p
+    .on('end', function() {
+        console.log('Processing finished successfully');
+        res.status(200).send('Processing finished successfully');
+    })
+    .on('error', function(err: any) {
+        console.log('An error occurred: ' + err.message);
+        res.status(500).send('An error occurred: ' + err.message);
+    })
+    .save(outputFilePath);
 });
 
-
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
-    console.log(`Video processing service listening at http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
